c781490e883579e538a24af31a75599ee4729314
nikilic <nikolailic02@gmail.com>
2019-06-01 16:35:01 +0200

    proba php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+    <?php include('header.html'); ?>
+    <h1>Hello World</h1>
+    <p>I'm hosted with GitHub Pages.</p>
+    <p>Proba</p>
+    <?php include('footer.php');?>
+</body>
+</html>
